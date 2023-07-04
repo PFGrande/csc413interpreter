d543f5e840caacb8f2efc8dc73be980689c23b06
Pedro <pgrande@mail.sfsu.edu>
2023-07-04 01:14:29 -0700

    declared remaining bytecode classes

--- interpreter/bytecodes/ByteCode.java
+++ interpreter/bytecodes/ByteCode.java
@@ -11,6 +11,20 @@ public interface ByteCode {
             case "LIT" -> new LitCode(args);
                 //public class litcode implements bytecode
             case "HALT" -> new HaltCode(args);
+            case "POP" -> new PopCode(args);
+            case "FALSEBRANCH" -> new FalseBranchCode(args);
+            case "GOTO" -> new GotoCode(args);
+            case "STORE" -> new StoreCode(args);
+            case "LOAD" -> new LoadCode(args);
+            case "ARGS" -> new ArgsCode(args);
+            case "CALL" -> new CallCode(args);
+            case "RETURN" -> new ReturnCode(args);
+            case "BOP" -> new BopCode(args);
+            case "READ" -> new ReadCode(args);
+            case "WRITE" -> new WriteCode(args);
+            case "LABEL" -> new LabelCode(args);
+            case "DUMP" -> new DumpCode(args);
+
             default -> throw new IllegalArgumentException();
         };
 
