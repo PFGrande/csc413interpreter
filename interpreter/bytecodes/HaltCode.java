677c6033819cb941c23ef74cb209631abcc3742a
Pedro <pgrande@mail.sfsu.edu>
2023-07-05 03:32:55 -0700

    modified halt's toString method to fix bug where dumping printed memory address

--- interpreter/bytecodes/HaltCode.java
+++ interpreter/bytecodes/HaltCode.java
@@ -10,4 +10,9 @@ public class HaltCode implements ByteCode { // do not dump
     public void execute(VirtualMachine vm) {
         vm.updateRunStatus(false);
     }
+
+    @Override
+    public String toString() {
+        return ""; // would print memory address if not for this return value
+    }
 }
